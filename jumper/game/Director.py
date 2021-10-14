48c56cae8269772c7f66b2db8c85461f789058ad
GerinWilde1 <91751688+GerinWilde1@users.noreply.github.com>
2021-10-14 09:46:17 -0500

    Add bits

---
+++ jumper/game/Director.py
@@ -0,0 +1,26 @@
+
+
+
+
+
+class Director:
+
+
+    def __init__(self):
+        pass
+        self.keep_playing = True
+
+        
+
+    def start_game(self):
+
+        while self.keep_playing:
+            self.get_inputs()
+            self.do_updates()
+            self.do_outputs()
+
+    def get_inputs(self):
+        pass
+    def do_outputs(self):
+        pass
+    def do_updates(self)
